d1baee0df11580b18d9f92534555ce030da3c695
Guru Prasad <guruprasad@Gurus-MacBook-Air.local>
2025-11-18 19:51:42 +0530

    working cli

--- autox/cli/root.py
+++ autox/cli/root.py
@@ -1,7 +1,12 @@
 import click
 
+# @click.command()
+# @click.option("--help", is_flag=True, help="Show this message and exit.")
+# @click.argument('name')
+# def cli(count, name):
+#     pass
+
 @click.command()
-@click.option("--help", is_flag=True, help="Show this message and exit.")
-@click.argument('name')
-def cli(count, name):
-    pass
+def cli():
+    """Prints a greeting."""
+    click.echo("Hello, World!")
