bd25389053992f1ee2a798b6e7e1a2f1ede81bb1
Guru Prasad <guruprasad@Gurus-MacBook-Air.local>
2025-11-18 19:42:06 +0530

    initial commit

--- autox/cli/root.py
+++ autox/cli/root.py
@@ -1,6 +1,7 @@
 import click
 
 @click.command()
-def cli():
-    """Prints a greeting."""
-    click.echo("Hello, World!")
+@click.option("--help", is_flag=True, help="Show this message and exit.")
+@click.argument('name')
+def cli(count, name):
+    pass
---
+++ autox/config.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from pathlib import Path
+import click
+from dotenv import dotenv_values, load_dotenv
+
+# Set autox root and environments directory paths
+AUTOX_ROOT = Path(__file__).resolve().parents[1]
+ENVIRONMENTS_DIR = Path(AUTOX_ROOT, "environments")
+
+
+class ConfigMap(Enum):
+    app_url = "APP_URL"
+    # AWS related configurations
+    aws_region = "AWS_REGION"
+    aws_availability_zone = "AWS_AVAILABILITY_ZONE"
+    aws_account_id = "AWS_ACCOUNT_ID"
+    eks_cluster_name = "EKS_CLUSTER_NAME"
+    # GitHub related configurations
+    github_token = "GITHUB_TOKEN"
+    github_repo_owner = "GITHUB_REPO_OWNER"
+    # Terraform repo related configurations
+    tf_github_repo = "TF_GITHUB_REPO"
+    tf_github_branch = "TF_GITHUB_BRANCH"
+
+
