97aa8a775707c03fa926dd91965401fbb029415c
Guru Prasad <guruprasad@Gurus-MacBook-Air.local>
2025-11-21 14:41:20 +0530

    initial commit - autox logger

--- autox/config.py
+++ autox/config.py
@@ -1,5 +1,9 @@
+import os
+
+import pydantic
 from enum import Enum
 from pathlib import Path
+from autox.autox_logger import logger
 
 # Set autox root and environments directory paths
 AUTOX_ROOT = Path(__file__).resolve().parents[1]
@@ -7,6 +11,7 @@ ENVIRONMENTS_DIR = Path(AUTOX_ROOT, "environments")
 
 
 class ConfigMap(Enum):
+    log_level = "LOG_LEVEL"
     app_url = "APP_URL"
     # AWS related configurations
     aws_region = "AWS_REGION"
@@ -19,3 +24,30 @@ class ConfigMap(Enum):
     # Terraform repo related configurations
     tf_github_repo = "TF_GITHUB_REPO"
     tf_github_branch = "TF_GITHUB_BRANCH"
+
+    def source(self, default=None, convert_to_bool=False, post_process=None):
+        env_variable_name = self.value
+
+        def default_factory():
+            env_variable_value = os.environ.get(env_variable_name, default)
+            
+            # Convert to bool if specified and the value is a string
+            if convert_to_bool and isinstance(env_variable_value, str):
+                env_variable_value = env_variable_value.upper() in {"TRUE", "YES", "1"}
+
+            # Posr process if specified and a function is provided
+            if post_process and env_variable_value:
+                env_variable_value = post_process(env_variable_value)
+
+            return env_variable_value
+
+        return pydantic.Field(default_factory=default_factory)
+    
+
+    def can_be_saved(self):
+        return not hasattr(self, "do_not_save")
+    
+
+# class Config:
+
+
---
+++ autox/utilities/common_utils.py
@@ -0,0 +1,6 @@
+from autox.autox_logger import logger
+
+
+def common_util():
+    logger.debug("Util log")
+    return
